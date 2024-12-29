8c1454461be01664ad22107df3df10ebc5ac731f
ritzvincentculanag <ritzvincentvergaraculanag@gmail.com>
2024-12-29 22:52:34 +0800

    initial commit

---
+++ hello-world/hello_world.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  print('Hello there!');
+  print('My creator is Arvee.');
+  print('He told me to say Hello, World!');
+  print('You can find him at github @ritzvincentculanag');
+  print('Good luck on your journey!');
+}
